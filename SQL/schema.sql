6d08c3b5b12274d41e1de85729d192c3275ffb13
Jonathan Bird <jonbird81@gmail.com>
2014-06-28 14:31:47 -0500

    Schema.sql sets up database with two tables

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,9 +3,19 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id MEDIUMINT NOT NULL AUTO_INCREMENT, 
+  user_id INTEGER(3),
+  text VARCHAR(140),
+  room_id INTEGER(3),
+  time_stamp TIMESTAMP,
+  PRIMARY KEY(id)
 );
 
+CREATE TABLE users (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(25),
+  PRIMARY KEY(id)
+);
 /* Create other tables and define schemas for them here! */
 
 
